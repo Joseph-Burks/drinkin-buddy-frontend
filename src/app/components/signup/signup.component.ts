c2eb2d09d49a9cd0c9f69dbf5f555893f6adbb09
Joseph-Burks <joe_burks_3@yahoo.com>
2022-03-09 02:19:53 -0600

    clean up sign up

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { Router } from "@angular/router";
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
-
-import { UserService } from '../../services/user.service';
 import { 
   passwordError,
   usernameError,
@@ -30,10 +25,6 @@ export class SignupComponent implements OnInit {
 
 
   constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private location: Location,
-    private userService: UserService,
     private _store: Store
   ) { }
 
@@ -50,21 +41,6 @@ export class SignupComponent implements OnInit {
     }
 
     this._store.dispatch(signUpUser(newUser))
-  //   this.userService.signUp(newUser)
-  //     .subscribe((userData) => {
-  //       if(userData.user){
-  //         localStorage.setItem('token', userData.token)
-  //         this._store.dispatch(logInUser(userData.user))
-  //         this.router.navigate(['/dashboard'])
-  //       } else {
-  //         if(userData.error.username){
-  //           this.usernameErrorMessage = `Username ${userData.error.username[0]}`
-  //         }
-  //         if(userData.error.password){
-  //           this.passwordErrorMessage = `Password ${userData.error.password[0]}`
-  //         }
-  //       }
-  //     });
   }
 
 }
